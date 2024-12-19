22dd48631d644bbbafaa083057ee4ea6ab6f928b
Kishan-Ramani <kishanramani269@gmail.com>
2024-12-19 18:24:13 +0530

    10 start

---
+++ 10ContextTodo/src/Contexts/TodoContext.js
@@ -0,0 +1,21 @@
+import { createContext, useContext } from "react";
+
+export const todoContext = createContext({
+    Todos : [
+        {
+            id:1,
+            todo : "Todo msg",
+            completed : false
+        }
+    ],
+    addTodo : (todo) => {},
+    updateTodo : (id,todo) => {},
+    deleteTodo : (id) => {},
+    completeTodo : (id) => {},
+})
+
+export const TodoProvider = todoContext.Provider
+
+export const useTodo = () => {
+    return useContext(todoContext)
+}
