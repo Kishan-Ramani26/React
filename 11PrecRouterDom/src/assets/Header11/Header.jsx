ef34e7875fd205f9a91ca18c1f49a9f9a4fd884b
Kishan-Ramani <kishanramani269@gmail.com>
2024-12-30 18:17:34 +0530

    on

--- 11PrecRouterDom/src/assets/Header11/Header.jsx
+++ 11PrecRouterDom/src/assets/Header11/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
 
 const Header = () => {
-  return (
-    <div className='h-10 w-screen pl-10 pt-5 text-white left-0 top-0 fixed '>
-        
-    </div>
-  )
+    return (
+        <div className='h-10 w-screen px-10 pt-5 text-white left-0 top-0 sticky z-50'>
+            <NavLink className={({isActive})=> 'hover:cursor-pointer'}>
+                Home
+            </NavLink>
+        </div>
+    )
 }
 
 export default Header
