e83abf16014007ddd2fd33f5c36b7458aac9673d
Kishan-Ramani <kishanramani269@gmail.com>
2025-01-01 18:11:50 +0530

    12 completed

--- 12PrecRouterDom/src/assets/Navbar/Navbar.jsx
+++ 12PrecRouterDom/src/assets/Navbar/Navbar.jsx
@@ -4,16 +4,14 @@ import { NavLink } from 'react-router-dom'
 const Navbar = () => {
     return (
         <>
-            <div className='w-screen bg-gray-500 text-white fixed top-0 left-0 z-50 flex items-center justify-center'>
-                <NavLink className={(isActive) => `text-2xl hover:cursor-pointer ${isActive ? "text-orange-500" : "text-gray-500"}`}>
+            <div className='w-screen h-15 bg-gray-100 text-white fixed top-0 left-0 z-50 flex items-center justify-start gap-14'>
+                <NavLink to="" className={({isActive}) => `text-2xl hover:cursor-pointer ${isActive ? "text-orange-500" : "text-gray-500"}`}>
                     Home
                 </NavLink>
-                <NavLink className={(isActive) => `text-2xl hover:cursor-pointer ${isActive ? "text-orange-500" : "text-gray-500"}`}>
+                <NavLink to="about" className={({isActive}) => `text-2xl hover:cursor-pointer ${isActive ? "text-orange-500" : "text-gray-500"}`}>
                     About
                 </NavLink>
-                <NavLink className={(isActive) => `text-2xl hover:cursor-pointer ${isActive ? "text-orange-500" : "text-gray-500"}`}>
-                    Contect
-                </NavLink>
+               
             </div>
         </>
     )
--- 12PrecRouterDom/src/main.jsx
+++ 12PrecRouterDom/src/main.jsx
@@ -2,9 +2,32 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './assets/Layout/Layout.jsx'
+import Home from './assets/Home/Home.jsx'
+import About from './assets/Home/About/About.jsx'
+
+
+
+const router = createBrowserRouter([{
+  path: "/",
+  element: <Layout />,
+  children: [
+    {
+      path: "",
+      element: <Home />
+    },
+    {
+      path:"about",
+      element:<About />
+    },
+  
+  ]
+}])
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
