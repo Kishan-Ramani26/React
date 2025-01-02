d50812cb3d0554493b39b508e3ed21b511634ab2
Kishan-Ramani <kishanramani269@gmail.com>
2025-01-02 16:53:29 +0530

    updated

--- 12PrecRouterDom/src/assets/Home/About/About.jsx
+++ 12PrecRouterDom/src/assets/Home/About/About.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <div className='h-screen w-screen top-0 left-0 bg-black flex items-center justify-center'>
+        <div className='text-5xl text-white'>About</div>
+      </div>
+    </>
   )
 }
 
--- 12PrecRouterDom/src/assets/Home/Home.jsx
+++ 12PrecRouterDom/src/assets/Home/Home.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <div className='h-screen w-screen top-0 left-0 bg-black flex items-center justify-center'>
+        <div className='text-5xl text-white'>Home</div>
+      </div>
+    </>
   )
 }
 
