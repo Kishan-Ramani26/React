92a3e72f8936c02bf4ced2001684a264271cd223
Kishan-Ramani <kishanramani269@gmail.com>
2025-01-03 17:20:26 +0530

    06added

--- 06CornuceConverter/src/hooks/CoranceyInfo.js
+++ 06CornuceConverter/src/hooks/CoranceyInfo.js
@@ -2,10 +2,14 @@ import { useEffect,useState } from "react";
 
 function useCoranceyInfo(currency){
     useEffect(() => {
+
+      const [data, setdata] = useState([])
+
       fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
-      .then(() => {
-        
-      })
-    }, [])
-    
+      .then((res) => res.json())
+      .then((res) => setdata(res[currency]))
+    }, [currency])
+    return data
 }
+
+export default useCoranceyInfo;
