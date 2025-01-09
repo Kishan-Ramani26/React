da278332e84444af66c13ebc7ea89fa99e2ec1bb
Kishan-Ramani <kishanramani269@gmail.com>
2025-01-09 17:42:07 +0530

    GSA

---
+++ GSAP_Prec/src/App.jsx
@@ -0,0 +1,23 @@
+import React, { useRef } from "react";
+import {useGSAP} from "@gsap/react";
+import gsap from "gsap";
+
+function App() {
+
+const reff = useRef();
+
+
+
+
+  return (
+    <>
+      <div className="h-screen w-full bg-black text-white">
+             <div className="box h-40 w-40 bg-red-700 rounded-md relative" >
+          
+             </div>
+      </div>
+    </>
+  )
+}
+
+export default App
