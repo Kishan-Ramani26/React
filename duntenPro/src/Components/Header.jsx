2cec20ccaad33cc43d3f85ad99cda9872248fb00
Kishan-Ramani <kishanramani269@gmail.com>
2025-01-17 16:49:02 +0530

    asdf

--- duntenPro/src/Components/Header.jsx
+++ duntenPro/src/Components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
                 <div className='relative text-black mix-blend-difference'>
                     <img src='https://www.duten.com/wp-content/themes/duten/static/images/logo-duten.svg' className='mix-blend-difference' />
                 </div>
-                <div className='relative flex items-center justify-between flex-col gap-2'>
+                <div className='relative flex items-center justify-between flex-col gap-2 cursor-pointer'>
                     <div className='h-[0.01vw] w-[5vw] bg-black '></div>
                     <div className='h-[0.01vw] w-[3vw] bg-black '></div>
                     <div className='h-[0.01vw] w-[5vw] bg-black '></div>
