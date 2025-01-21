85130cd74299aaa8021c06ad246bbb169f609792
Kishan-Ramani <kishanramani269@gmail.com>
2025-01-21 11:30:27 +0530

    asd

---
+++ reactrouterDomPrec/Components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link,NavLink } from 'react-router-dom'
+
+
+const Header = () => {
+  return (
+    <div className='w-full h-11 bg-cyan-300'>
+        {/* <NavLink to="/">Home</NavLink>
+        <NavLink>Home</NavLink>
+        <NavLink>Home</NavLink>
+         */}
+        
+        
+    </div>
+  )
+}
+
+export default Header
