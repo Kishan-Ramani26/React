4914de760c1390c56610c466778abd38dfdf3b57
Kishan-Ramani <kishanramani269@gmail.com>
2025-01-29 18:09:53 +0530

    Spline demo

---
+++ splineDemo/src/Spline.jsx
@@ -0,0 +1,11 @@
+import Spline from '@splinetool/react-spline/next';
+
+export default function Home() {
+  return (
+    <main>
+      <Spline
+        scene="https://prod.spline.design/z0NBSDix1j6bHkrM/scene.splinecode" 
+      />
+    </main>
+  );
+}
