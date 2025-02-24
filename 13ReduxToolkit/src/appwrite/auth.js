fc67dbc95546d027b3987e1fa049f2d7b6dcb3c6
Kishan-Ramani <kishanramani269@gmail.com>
2025-02-24 18:41:30 +0530

    Add global styles and implement createAccount method in AuthService

--- 13ReduxToolkit/src/appwrite/auth.js
+++ 13ReduxToolkit/src/appwrite/auth.js
@@ -10,9 +10,23 @@ export class AuthService{
         this.client
             .setEndpoint(config.appwriteURL)
             .setProject(config.appwriteProjectID);
+
         this.account = new Account(this.client);
     }
 
+    async createAccount({email,password,name}){
+        try {
+            const userAccount = await this.client.account.create(ID.unique(),email,password,name);
+            if (userAccount) {
+                return userAccount;
+            } else {
+                error = new Error("User account not created");
+                throw error;
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
 }
 
 const authService = new AuthService();
