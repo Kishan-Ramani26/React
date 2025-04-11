e4d8038c7df9f8ed39a65f4e1d36e3577c4db27d
Kishan-Ramani <kishanramani269@gmail.com>
2025-04-11 19:36:15 +0530

    Refactor App component to include form fields for name and email input

--- formValidation/src/App.jsx
+++ formValidation/src/App.jsx
@@ -1,14 +1,31 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-  
-
   return (
-  <>
-  <h1 className='bg-red-500 '>Hello</h1>
-  </>
-  )
+    <>
+      <div className="h-screen w-full bg-black text-white flex justify-center items-center">
+        <form className=" border-2 border-white px-10 py-10 flex items-start justify-center flex-col gap-2">
+          <label htmlFor="name">Name : </label>
+          <input
+            type="text"
+            placeholder="Enter your name"
+            name="name"
+            id="name"
+          />
+          <br />
+          <label htmlFor="email">Email : </label>
+          <input
+            type="email"
+            placeholder="Enter your Email"
+            name="email"
+            id="email"
+          />
+          <br />
+          
+        </form>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
