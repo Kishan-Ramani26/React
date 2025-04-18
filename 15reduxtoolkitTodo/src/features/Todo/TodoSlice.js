3fbfce0b95c2999ca93e6bbed7bc63e3fa935d40
Kishan-Ramani <kishanramani269@gmail.com>
2025-04-18 17:24:21 +0530

    Refactor TodoSlice to improve structure and readability; implement addTodo, removeTodo, and updateTodo reducers

--- 15reduxtoolkitTodo/src/features/Todo/TodoSlice.js
+++ 15reduxtoolkitTodo/src/features/Todo/TodoSlice.js
@@ -2,5 +2,36 @@ import {createSlice , nanoid} from "@reduxjs/toolkit"
 // nanoid genert only unic id
 
 const initialState = {
-    
+    todos:[{id:1 , text:"Hello world"}]
 }
+
+
+export const todoSlice = createSlice({
+    name:todo,
+    initialState,
+    reducers:{
+        addTodo:(state,action) => {
+            const todo = {
+                id: nanoid(),
+                text:action.payload
+            }
+            state.todos.push(todo)
+        },
+
+        removeTodo:(state,action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+        },
+
+        updateTodo:(state,action) => {
+            state.todos = state.todos.filter((todo)=>{
+                if(todo.id == action.payload){
+                    todo.text = action.payload.text
+                }
+            })
+        }
+    }
+})
+
+
+
+
