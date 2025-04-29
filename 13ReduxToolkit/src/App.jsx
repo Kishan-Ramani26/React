cbfdb011a14646a1d83d342b218e38ff5c5a2e3b
Kishan-Ramani <kishanramani269@gmail.com>
2025-04-29 17:58:30 +0530

    Integrate authentication with Redux; update App component to manage user state and loading, refactor authSlice, and configure store in main entry point.

--- 13ReduxToolkit/src/App.jsx
+++ 13ReduxToolkit/src/App.jsx
@@ -1,20 +1,60 @@
 import "./App.css";
-import {useDispatch} from "react-redux";
+import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
 import { Footer } from "./Components";
 import Header from "./Components/Header/Header";
+import authService from "./appwrite/auth";
+import { login, logout } from "./Store/authSlice";
+import {Outlet} from "react-router-dom"
 
 function App() {
   console.log(import.meta.env.VITE_APPWRITE_URL);
 
+  const [loading, setloading] = useState(true);
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setloading(false));
+  }, []);
 
-  return (
-    <>
-      <Header />
-      <h1>Blog App</h1>
-      <Footer />
-    </>
-  );
+  // if (loading != true) {
+  //   return null;
+  // } else {
+  //   return (
+  //     <div className="min-h-screen flex items-center justify-center flex-col">
+  //       <div className="h-screen w-full flex items-center justify-between flex-col ">
+  //         <Header />
+  //         <main>
+  //           <Outlet />
+  //         </main>
+  //         <Footer />
+  //       </div>
+  //     </div>
+  //   )
+  // }
+
+  return !loading ? (
+    <div className="min-h-screen flex items-center justify-center flex-col">
+            <div className="h-screen w-full flex items-center justify-between flex-col ">
+              <Header />
+              <main>
+                {/* < Outlet /> */}
+              </main>
+              <Footer />
+            </div>
+          </div> 
+  )
+  : null
+
 }
 
 export default App;
