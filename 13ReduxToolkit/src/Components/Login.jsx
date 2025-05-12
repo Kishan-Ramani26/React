42072c2972c02f584c4e531651a3cec3b41c6495
Kishan-Ramani <kishanramani269@gmail.com>
2025-05-12 18:28:58 +0530

    Refactor Login component; implement useNavigate and useDispatch hooks, enhance error handling, and set up form submission logic with react-hook-form.

--- 13ReduxToolkit/src/Components/Login.jsx
+++ 13ReduxToolkit/src/Components/Login.jsx
@@ -1,11 +1,28 @@
 import React,{useState} from 'react'
-import { Link,Navigate } from 'react-router-dom'
-import { login } from '../Store/authSlice'
+import { Link,Navigate, useNavigate } from 'react-router-dom'
+import { login as authlogin } from '../Store/authSlice'
 import {Button , Input , Logo} from './index';
 import authService from '../appwrite/auth';
 import {useForm} from "react-hook-form"
+import { useDispatch } from 'react-redux';
 
 function Login() {
+
+    const navigate = useNavigate();
+    const Dispatch = useDispatch();
+    const {register,handleSubmit} = useForm()
+    const [error, seterror] = useState("");
+
+    const login = async(data) =>{
+        seterror("");
+        try {
+            const session = await authService.login(data)
+            
+        } catch (error) {
+            seterror(error.message)
+        }
+    }
+
   return (
     <div>Login</div>
   )
