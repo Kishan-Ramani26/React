dd4afcdcd20279ea8340cf961f5b41344c67999e
Kishan-Ramani <kishanramani269@gmail.com>
2025-05-14 17:22:21 +0530

    Implement SignUp component with form handling and validation; enhance user experience with improved UI and error messaging.

---
+++ 13ReduxToolkit/src/Components/AuthLayout.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+export default function Protected({ children, authentication = true }) {
+  const Navigate = useNavigate();
+  const authStatus = useSelector((state) => state.auth.status);
+  const [loader, setloader] = useState(true);
+
+  useEffect(() => {
+
+    // true && false !== true -it means user is not authenticate
+
+    if (authentication && authStatus !== authentication) {
+      Navigate("/login");
+    } 
+    // false && true !== true -it means user is authenticate
+
+    else if (!authentication && authStatus !== authentication) {
+      Navigate("/");
+    }
+    setloader(false);
+  }, [authentication, Navigate, authStatus]);
+
+  return loader ? <h1>Loading...</h1> : <>{children}</>;
+}
