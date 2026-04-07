a2446b4b87b149626852267b23aec86668262d04
Kishan-Ramani <kishanramani269@gmail.com>
2026-04-07 10:02:14 +0530

    Adjust smoothTouch and section class names
    Updated smoothTouch value and modified section classes for better styling.

--- 17dogstudio/src/App.jsx
+++ 17dogstudio/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       content: "#smooth-content",
       smooth: 1.3,
       effects: true,
-      smoothTouch: 0.1,
+      smoothTouch: 0.5,
       ease: "power3.out",
     });
     return () => smoother.kill();
@@ -35,11 +35,17 @@ function App() {
       </Canvas>
       <div id="smooth-wrapper" className=" w-full relative">
         <div id="smooth-content" className=" w-full">
-          <section className="h-screen w-full flex items-center justify-center">
+          <section className="section-1 h-screen w-full flex items-center justify-center">
             <h1 className="text-4xl font-bold text-white">Dog Studio</h1>
           </section>
 
-          <section className="h-screen w-full flex items-center justify-center">
+          <section className="section-2 h-screen w-full flex items-center justify-center">
+            <h1 className="text-4xl font-bold text-white">
+              Scroll to see the effect
+            </h1>
+          </section>
+
+          <section className="section-3 h-screen w-full flex items-center justify-center">
             <h1 className="text-4xl font-bold text-white">
               Scroll to see the effect
             </h1>
